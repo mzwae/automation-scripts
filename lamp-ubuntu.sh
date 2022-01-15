b28e1a4ac25c2f782ab62c99f151f481b5e4a349
mzwae <mrmonjaie@gmail.com>
2022-01-15 22:07:32 +0000

    Adds basic lamp server packages

---
+++ lamp-ubuntu.sh
@@ -0,0 +1,15 @@
+# Update the package manager cache
+sudo apt update 
+
+# Install apache
+sudo apt install apache2 -y
+
+# Install mariadb server
+sudo apt install mariadb-server -y
+
+# Install php
+sudo apt install php libapache2-mod-php php-mysql -y
+
+# Install phpmyadmin
+sudo apt install phpmyadmin
+
