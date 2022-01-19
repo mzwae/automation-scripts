db9a0fa3e537c2902ad7de8aecc7b73b1493cc1b
mzwae <mrmonjaie@gmail.com>
2022-01-19 20:35:27 +0000

    Adds creating virtual host automation script

---
+++ add-vh.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+read -p "Enter your site name. " -e -i 'mysite.com.conf' sitename
+read -p "Enter your site directory name. It should be saved in /var/www: " -e -i 'mysite'  directory
+read -p "Enter your site domain name.  " -e -i 'mysite.com' domain
+read -p "Enter site admin email.  " -e -i 'webmaster@localhost' email
+
+#touch | sudo tee /etc/apache2/sites-available/"$sitename"
+
+echo "<VirtualHost *:80>
+
+
+        ServerAdmin $email
+        DocumentRoot /var/www/$directory
+
+        ServerName $domain
+        ServerAlias www.$domain
+
+
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+</VirtualHost> " | sudo tee  /etc/apache2/sites-available/"$sitename"
