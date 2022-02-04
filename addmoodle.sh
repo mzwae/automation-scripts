be1793789f915096e22a5a80e20ade857285ffee
mzwae <mrmonjaie@gmail.com>
2022-02-04 23:07:01 +0000

    Fixes a typo

--- addmoodle.sh
+++ addmoodle.sh
@@ -1,6 +1,6 @@
 # This is script is to automate downloading moodle and setting up virtual host to Apache Linux server
 
-# Download and Install Wordpress
+# Download and Install moodle
 cd /var/www
 sudo wget http://sourceforge.net/projects/moodle/files/Moodle/stable24/moodle-latest-24.tgz
 sudo tar -zxvf moodle-latest-24.tgz
