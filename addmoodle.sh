a5a4e77e7a03592e475f6a4f568c0e8d329b85c5
mzwae <mrmonjaie@gmail.com>
2022-02-05 23:00:43 +0000

    Update moodle download link to git repo to get the latestest version

--- addmoodle.sh
+++ addmoodle.sh
@@ -2,10 +2,13 @@
 
 # Download and Install moodle
 cd /var/www
-sudo wget http://sourceforge.net/projects/moodle/files/Moodle/stable24/moodle-latest-24.tgz
-sudo tar -zxvf moodle-latest-24.tgz
-sudo rm moodle-latest-24.tgz
+# sudo wget http://sourceforge.net/projects/moodle/files/Moodle/stable24/moodle-latest-24.tgz
+# sudo wget https://github.com/moodleou/moodle-qtype_opaque/zipball/master
+# sudo tar -zxvf moodle-latest-24.tgz
+# sudo rm moodle-latest-24.tgz
 
-cd /var/www
-sudo chown -R www-data:www-data moodle
-sudo chmod 755 -R moodle
+# cd /var/www
+# sudo chown -R www-data:www-data moodle
+# sudo chmod 755 -R moodle
+
+git clone git@github.com:moodle/moodle.git
