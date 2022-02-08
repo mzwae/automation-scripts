4203ce2912f96ac4a21ef330ea3c21409c1022e6
mzwae <mrmonjaie@gmail.com>
2022-02-08 22:25:22 +0000

    Code Cleanup

--- addmoodle.sh
+++ addmoodle.sh
@@ -1,16 +1,9 @@
-# This is script is to automate downloading moodle and setting up virtual host to Apache Linux server
+# This script is to automate downloading moodle and setting up virtual host to Apache Linux server
 
-# Download and Install moodle
 cd /var/www
-# sudo wget http://sourceforge.net/projects/moodle/files/Moodle/stable24/moodle-latest-24.tgz
-# sudo wget https://github.com/moodleou/moodle-qtype_opaque/zipball/master
-# sudo tar -zxvf moodle-latest-24.tgz
-# sudo rm moodle-latest-24.tgz
 
 sudo mkdir moodledata
 
 sudo git clone git@github.com:moodle/moodle.git
 
-# cd /var/www
 sudo chown -R www-data:www-data moodle moodledata
-sudo chmod 755 -R moodle moodledata
